1714ed4bf4a1f0443d6e8a97d12fec8def67fef3
ducnghiem2411 <ducnghiem2411@gmail.com>
2019-12-13 15:58:36 +0700

    1st deploy

---
+++ main.js
@@ -0,0 +1,279 @@
+
+function autocomplete(inp, arr) {
+
+  //the autocomplete function takes two arguments
+  //the text field element and an array of possible autocompleted values:
+
+  var currentFocus;
+
+  //Execute a function when someone writes in the text field
+
+  inp.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+
+    //Close any already open lists of autocompleted values
+    closeAllLists();
+    if (!val) { return false; }
+    currentFocus = -1;
+
+    //create a DIV element that will contain the items (values)
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+
+    //append the DIV element as a child of the autocomplete container
+    this.parentNode.appendChild(a);
+
+    //set the limit for dropdown result
+    var limit = 0;
+
+    //for each item in the array
+    for (i = 0; i < arr.length; i++) {
+      //check if the item starts with the same letters as the text field value
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+        //create a DIV element for each matching element
+        b = document.createElement("DIV");
+
+        //make the matching letters bold
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+
+        //insert a input field that will hold the current array item's value
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+        //execute a function when someone clicks on the item value (DIV element)
+        b.addEventListener("click", function (e) {
+
+          //insert the value for the autocomplete text field
+          inp.value = this.getElementsByTagName("input")[0].value;
+
+          //close the list of autocompleted values, or any other open lists of autocompleted values
+          closeAllLists();
+        });
+        if (limit <= 4) {
+          a.appendChild(b);
+          limit += 1;
+        }
+      }
+    }
+
+  });
+
+  //execute a function presses a key on the keyboard
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      //If the arrow DOWN key is pressed, increase the currentFocus variable
+      currentFocus++;
+      //and and make the current item more visible
+      addActive(x);
+    } else if (e.keyCode == 38) { //up
+      //If the arrow UP key is pressed, decrease the currentFocus variable
+      currentFocus--;
+      //Make the current item more visible
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      //If the ENTER key is pressed, prevent the form from being submitted
+      e.preventDefault();
+      if (currentFocus > -1) {
+        //and simulate a click on the "active" item
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+
+  function closeAllLists(elmnt) {
+    //close all autocomplete lists in the document, except the one passed as an argument
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+
+  // execute a function when someone clicks in the document
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+
+}
+
+//initiate the autocomplete function
+autocomplete(document.getElementById("input"), monsters);
+
+let searchBtn = document.getElementById("search-btn");
+searchBtn.onclick = search;
+
+let inputForm = document.getElementById("input");
+inputForm.addEventListener("keyup", function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("search-btn").click();
+  }
+});
+
+function showLocationData(monsterName) {
+  let obj = model[`${monsterName}`]
+  let result = `<h4>${obj.name}</h4>`
+  for (const key in obj) {
+    if (key != "name") {
+      let details = `<p>${key} has [ ${obj[key]} ]</p>`
+      result += details;
+    }
+  }
+  document.getElementById("result").innerHTML = result;
+}
+
+
+function search(monster) {
+  document.getElementById("result").innerHTML = "";
+  monster = document.getElementById("input").value
+  stringConverterAndRender(monster);
+}
+
+function stringConverterAndRender(str) {
+  //Remove all white space of string got from input
+  let monsterName = str.replace(/ /g, '').toLowerCase()
+
+  //Get location data from model by input
+  if (model[`${monsterName}`]) {
+    showLocationData(monsterName);
+  }
+
+  //Get location data by tip
+  if (monsterName == "kite" || monsterName == "blueskin") {
+    showLocationData("blueimp");
+  }
+  if (monsterName == "tears" || monsterName == "rain") {
+    showLocationData("ameonna");
+  }
+  if (monsterName == "fisherman" || monsterName == "sea" || monsterName == "beard" || monsterName == "mustache" || monsterName == "cane" || monsterName == "mustache") {
+    showLocationData("umibozu");
+  }
+  if (monsterName == "butt" || monsterName == "slapping" || monsterName == "redghost") {
+    showLocationData("redimp");
+  }
+  if (monsterName == "drum") {
+    showLocationData("yellowimp");
+  }
+  if (monsterName == "moss" || monsterName == "wall") {
+    showLocationData("norikabe");
+  }
+  if (monsterName == "seductive" || monsterName == "redtail" || monsterName == "cherrytree") {
+    showLocationData("mio");
+  }
+  if (monsterName == "orb" || monsterName == "horn" || monsterName == "fortune" || monsterName == "luck" || monsterName == "luckwealth") {
+    showLocationData("zashiki");
+  }
+  if (monsterName == "bubbles") {
+    showLocationData("koi");
+  }
+  if (monsterName == "waterball" || monsterName == "waterlily" || monsterName == "river" || monsterName == "leaf") {
+    showLocationData("kappa");
+  }
+  if (monsterName == "offering") {
+    showLocationData("oguna");
+  }
+  if (monsterName == "feathercoat" || monsterName == "younggirl") {
+    showLocationData("doujo");
+  }
+  if (monsterName == "poison" || monsterName == "bugs") {
+    showLocationData("kodokushi");
+  }
+  if (monsterName == "naginata") {
+    showLocationData("karasutengu");
+  }
+  if (monsterName == "dandelion" || monsterName == "healing" || monsterName == "ding") {
+    showLocationData("kusa");
+  }
+  if (monsterName == "armor" || monsterName == "harden" || monsterName == "petrified") {
+    showLocationData("samuraix");
+  }
+  if (monsterName == "scarecrow" || monsterName == "cursenail" || monsterName == "curseddart" || monsterName == "strawdoll") {
+    showLocationData("ushinotoki");
+  }
+  if (monsterName == "stonebuddha" || monsterName == "diamond" || monsterName == "sutra") {
+    showLocationData("hitotsume");
+  }
+  if (monsterName == "shell") {
+    showLocationData("shuzu");
+  }
+  if (monsterName == "minidrum" || monsterName == "littlefairy" || monsterName == "cute" || monsterName == "drum") {
+    showLocationData("chocho");
+  }
+  if (monsterName == "stonehammer" || monsterName == "strong") {
+    showLocationData("yamawaro");
+  }
+  if (monsterName == "mahjong" || monsterName == "cheat" || monsterName == "gambling" || monsterName == "ceramic") {
+    showLocationData("jikikaeru");
+  }
+  if (monsterName == "bird" || monsterName == "house" || monsterName == "protection" || monsterName == "purpledog") {
+    showLocationData("inugami");
+  }
+  if (monsterName == "bone" || monsterName == "hatred") {
+    showLocationData("honeonna");
+  }
+  if (monsterName == "candles" || monsterName == "coffins") {
+    showLocationData("kyonshiani");
+  }
+  if (monsterName == "puppet" || monsterName == "control") {
+    showLocationData("puppeteer");
+  }
+  if (monsterName == "abeautifulfairy" || monsterName == "accessories") {
+    showLocationData("momiji");
+  }
+  if (monsterName == "bell" || monsterName == "nightmare") {
+    showLocationData("yumekui");
+  }
+  if (monsterName == "dance" || monsterName == "flower") {
+    showLocationData("momo");
+  }
+  if (monsterName == "soupbowl" || monsterName == "guitar" || monsterName == "teeth") {
+    showLocationData("mouba");
+  }
+  if (monsterName == "blood" || monsterName == "bats") {
+    showLocationData("vampira")  
+  }
+  if (monsterName == "dagger" || monsterName == "blackscythe" || monsterName == "tantosword") {
+    showLocationData("kuromujou");
+  }
+  if (monsterName == "white" || monsterName == "takelife") {
+    showLocationData("shiromujou");
+  }
+  if (monsterName == "hammer" || monsterName == "plow" || monsterName == "rake") {
+    showLocationData("kamaitachi");
+  }
+  if (monsterName == "paperfan" || monsterName == "scholar") {
+    showLocationData("youko");
+  }
+  if (monsterName == "bigwings" || monsterName == "flute") {
+    showLocationData("ootengu");
+  }
+  if (monsterName == "ghostmask" || monsterName == "hell") {
+    showLocationData("enma");
+  }
+}
+
+
+
---
+++ model.js
@@ -0,0 +1,466 @@
+const model = {};
+
+//An array containing all the monsters and tips
+var monsters = [
+    "A beautiful fairy", "Accessories", "Ame Onna", "Armor", "Akashita", "Arakawa Lord",
+    "Bats", "Beard", "Bell", "Big Wings", "Bird", "Black Panther", "Black Scythe", "Blade", "Blood", "Blue Imp", "Blue skin", "Bone", "Bubbles", "Bugs", "Butt",
+    "Candles", "Cane", "Ceramic", "Cheat", "Cherry Tree", "Chocho", "Cloud", "Coffins", "Control", "Curse Nail", "Cursed Dart", "Cute",
+    "Dagger", "Dance", "Dandelion", "Diamond", "Ding", "Doll", "Doujo", "Drum",
+    "Enma",
+    "Fairy", "Feather coat", "Fisherman", "Flower", "Flute", "Fortune",
+    "Gaki", "Gambling", "Ghost Mask", "Girl", "Grave Digger", "Green Imp", "Guitar",
+    "Hahakigami", "Hammer", "Hangan", "Harden", "Hatred", "Healing", "Hitotsume", "Hone Onna", "Horn", "Hououka", "House",
+    "Inugami",
+    "Jikikaeru",
+    "Kamaitachi", "Kamikui", "Kanko", "Kappa", "Karasu Tengu","Kite","Kodokushi", "Koi", "Kubinashi", "Kuro Mujou", "Kusa", "Kyonshi Ani", "Kyonshi Imoto", "Kyuumei Neko",
+    "Lantern Boy", "Lantern Soul", "Leaf", "Lily", "Little Fairy", "Luck", "Luck Wealth",
+    "Mahjong", "Mini Drum", "Mio", "Momiji", "Momo", "Moss", "Mouba", "Mustache",
+    "Naginata", "Nail", "Nightmare", "Nurikabe",
+    "Offering", "Oguna", "Ootengu", "Orb",
+    "Paper Fan", "Parasite", "Petrified", "Pilow", "Poison", "Protection", "Puppet", "Puppeteer", "Purple Dog",
+    "Rain", "Rake", "Red Ghost", "Red Imp", "Red Tail", "River",
+    "Sakura", "Samurai X", "Satori", "Scarecrow", "Scholar", "Scythe", "Sea", "Seductive", "Shell", "Shiro Mujou", "Shuten Doji", "Shuzu",
+    "Slapping", "Soup Bowl", "Staff", "Stone Buddha", "Stone hammer", "Straw Doll", "Strong", "Sutra",
+    "Take life", "Tanto Sword", "Tanuki", "Tears", "Teeth", "Tesso", "Tree",
+    "Umibozu", "Umbrella", "Ushi no Toki",
+    "Vampira",
+    "Wall", "Water Lily", "Water ball", "Wealth", "White", "Wind",
+    "Yamausagi", "Yamawaro", "Yellow Imp", "Youko", "Young Girl", "Yuki Onna", "Yumekui",
+    "Zashiki", "Zombie Dog",
+];
+
+model.lanternsoul = {
+    "name": "Lantern Soul",
+    "Chapter 9 1st Lantern Boy": 3,
+    "Arakawa Stage 1": 3,
+    "Arakawa Stage 2": 3,
+    "Arakawa Stage 3": 3,
+    "Shishio Stage 2" : 3,
+    "Chapter 2 2nd Grave Digger" : 2,
+    "Chapter 2 1st Parasite" : 2,
+    "Chapter 9 2nd Lantern Boy" : 1,
+}
+
+model.akashita = {
+    "name": "Akashita",
+    "Chapter 23 1st Hone Onna": 3,
+    "Chapter 23 2nd Hone Onna": 3,
+    "Shishio Stage 4": 3,
+    "Kamaitachi Stage 1": 3,
+}
+
+model.lanternboy = {
+    "name" : "Lantern Boy",
+    "Kappa Stage 1" : 3,
+    "Kappa Stage 2": 3,
+    "Kamaitachi Stage 1": 3,
+    "Chapter 9 1st Lantern Boy" : 1,
+}
+
+model.redimp = {
+    "name" : "Red Imp",
+    "Chapter 13 1st Gaki" : 3,
+    "Chapter 13 2nd Gaki" : 3,
+    "Chapter 13 3rd Gaki" : 3,
+}
+
+model.greenimp = {
+    "name" : "Green Imp",
+    "Chapter 6 1st Blue Imp" : 3,
+    "Chapter 6 2nd Blue Imp" : 3,
+    "Chapter 6 boss Kodokushi" : 3,
+}
+
+model.yellowimp = {
+    "name" : "Yellow Imp",
+    "Shishio Stage 1" : 3,
+    "Chapter 8 1st Green Imp" : 2,
+    "Chapter 8 2nd Green Imp" : 2,
+}
+
+model.hahakigami = {
+    "name" : "Hahakigami",
+    "Ootengu Stage 1" : 4,
+    "Chapter 6 1st Lantern Soul" : 3,
+    "Chapter 6 2nd Lantern Soul" : 3,
+}
+
+model.blueimp = {
+    "name" : "Blue Imp",
+    "Yoto Hime Stage 1" : 4,
+    "Chapter 10 1st Ushi no Toki" : 2,
+    "Chapter 10 2nd Ushi no Toki" : 2,
+}
+
+model.parasite = {
+    "name" : "Parasite",
+    "Chapter 16 1st Akashita" : 3,
+    "Chapter 16 2nd Akashita" : 3,
+    "Chapter 16 3rd Akashita" : 3,
+    "Arakawa Stage 1" : 3,
+
+}
+
+model.umbrella = {
+    "name" : "Umbrella",
+    "Ame Onna Stage 4" : 3,
+    "Chapter 4 (Hard) 2nd Hahakigami" : 3,
+}
+
+model.gravedigger = {
+    "name" : "Grave Digger",
+    "Ame Onna Stage 1" : 3,
+    "Shishio Stage 1" : 2,
+}
+
+model.nurikabe = {
+    "name" : "Nurikabe",
+    "Chapter 14 1st Nurikabe" : 6,
+    "Chapter 14 2nd Nurikabe" : 6,
+    "Chapter 14 3rd Nurikabe" : 6,
+}
+
+model.blackpanther = {
+    "name" : "Black Panther",
+    "Kappa Stage 4" : 3,
+}
+
+model.zombiedog = {
+    "name" : "Zombie Dog",
+    "Kiyohime Stage 1" : 13,
+}
+
+model.mio = {
+    "name" : "Mio",
+    "Ootengu Stage 1" : 4,
+    "Yoto Hime Stage 4" : 3,
+    "Chapter 18 1st Mio" : 2,
+    "Chapter 18 2nd Mio" : 2,
+    "Chapter 18 3rd Mio" : 2,
+}
+
+model.zashiki = {
+    "name" : "Zashiki",
+    "Ame Onna Stage 1" : 3,
+    "Chapter 10 2nd Satori" : 2,
+    "Chapter 10 2nd Puppeteer" : 2
+}
+
+model.koi = {
+    "name" : "Koi",
+    "Chapter 7 1st Koi" : 3,
+    "Chapter 7 2nd Lantern Boy" : 2,
+    "Chapter 7 2nd Koi" : 1,
+}
+
+model.kyuumeineko = {
+    "name" : "Kyuumei Neko",
+    "Chapter 18 1st Kyuumei Neko" : 6,
+    "Chapter 18 2nd Kyuumei Neko" : 6,
+    "Chapter 18 3rd Kyuumei Neko" : 6,
+}
+
+model.tanuki = {
+    "name" : "Tanuki",
+    "Yoto Hime Stage 1" : 4,
+    "Chapter 10 1st Satori" : 3,
+    "Chapter 10 1st Puppeteer" : 2,
+}
+
+model.kappa = {
+    "name" : "Kappa",
+    "Umibozu Stage 5" : 5,
+    "Umibozu Stage 3" : 3,
+}
+
+model.oguna = {
+    "name" : "Oguna",
+    "Kappa Stage 1" : 3,
+    "Aoandon Stage 3" : 3,
+    "Chapter 12 1st Oguna" : 1,
+    "Chapter 12 2nd Oguna" : 1,
+
+}
+
+model.doujo = {
+    "name" : "Doujo",
+    "Chapter 3 2nd Yellow Imp" : 3,
+    "Chapter 3 1st Samurai X" : 2,
+    "Chapter 3 2nd Akashita" : 2,
+}
+
+model.gaki = {
+    "name" : "Gaki",
+    "Chapter 11 2nd Gaki" : 3,
+    "Chapter 11 1st Gaki" : 1,
+    "Soul Stage 8" : 1,
+}
+
+model.kodokushi = {
+    "name" : "Kodokushi",
+    "Kappa Stage 3" : 3,
+}
+
+model.karasutengu = {
+    "name" : "Karasu Tengu",
+    "Chapter 12 2nd Umibozu" : 2,
+    "Chapter 12 1st Oguna" : 2,
+    "Chapter 12 2nd Oguna" : 2,
+}
+
+model.kamikui = {
+    "name" : "Kamikui",
+    "Kappa Stage 3" : 3,
+    "Soul Stage 7" : 1,
+    "Chapter 21 1st Kamikui" : 1,
+    "Chapter 21 2nd Kamikui" : 1,
+    "Chapter 21 3rd Kamikui" : 1,
+
+}
+
+model.kusa = {
+    "name" : "Kusa",
+    "Soul Sougenbi" : 4,
+}
+
+model.warriorsoul = {
+    "name" : "Warrior Soul",
+    "Chapter 25 2nd Kodokushi" : 2,
+    "Chapter 11 1st Warrior Soul" : 1,
+    "Chapter 11 2nd Warrior Soul" : 1,
+}
+
+model.kyonshiimoto = {
+    "name" : "Kyonshi Imoto",
+}
+
+model.samuraix = {
+    "name" : "Samurai X",
+    "Chapter 3 1st Samurai X" : 1,
+    "Chapter 3 2nd Samurai X" : 1,
+    "Soul Sougenbi" : 1,
+}
+
+model.ushinotoki = {
+    "name" : "Ushi no Toki",
+    "Chapter 21 1st Kamikui" : 3,
+    "Chapter 21 2nd Kamikui" : 2,
+    "Chapter 21 3rd Kamikui" : 2,
+    "Kappa Stage 4" : 2, 
+}
+
+model.hitotsume = {
+    "name" : "Hitotsume",
+    "Chapter 11 2nd Warrior Soul" : 3,
+    "Chapter 11 1st Hitotsume (Hard)" : 2,
+    "Chapter 11 2nd Hitotsume (Hard)" : 2,
+}
+
+model.tesso = {
+    "name" : "Tesso",
+    "Chapter 9 2nd Lantern Boy" : 2,
+    "Chapter 9 2nd Tesso" : 2,
+    "Chapter 9 2nd Yamausagi" : 1,
+    "Chapter 9 1st Tesso" : 1,
+    "Coin Monster" : 16,
+}
+
+
+model.shuzu = {
+    "name" : "Shuzu",
+    "Chapter 25 1st Samurai X" : 1,
+    "Chapter 25 2nd Samurai X" : 1,
+}
+
+model.ameonna = {
+    "name" : "Ame Onna",
+    "Yoto Hime Stage 3" : 3,
+    "Umibozu Stage 5" : 3,
+}
+
+model.kanko = {
+    "name" : "Kanko",
+    "Momiji Stage 1" : 4,
+    "Chapter 5 1st Kanko" : 2,
+    "Chapter 5 2nd Kanko" : 1,
+}
+
+model.yamausagi = {
+    "name" : "Yamausagi",
+    "Chapter 9 1st Yamausagi" : 4,
+    "Chapter 9 2nd Yamausagi" : 3,
+}
+
+model.chocho = {
+    "name" : "Chocho",
+    "Yoto Hime Stage 2" : 3,
+    "Chapter 6 1st Chocho" : 1,
+    "Chapter 6 2nd Chocho" : 1,
+}
+
+model.yamawaro = {
+    "name" : "Yamawaro",
+    "Kappa Stage 2" : 3,
+    "Chapter 16 1st Gaki" : 2,
+    "Chapter 16 2nd Gaki" : 2,
+    "Chapter 16 3rd Gaki" : 2,
+
+}
+
+model.kubinashi = {
+    "name" : "Kubinashi",
+    "Ame Onna Stage 2" : 3,
+    "Soul Stage 11" : 1,
+}
+
+model.satori = {
+    "name" : "Satori",
+    "Momiji Stage 1" : 3,
+    "Kappa Stage 4" : 3,
+    "Soul 10" : 1,
+}
+
+model.jikikaeru = {
+    "name" : "Jikikaeru",
+    "Aoandon Stage 5" : 2,
+}
+
+model.yukionna = {
+    "name" : "Yuki Onna",
+    "Arakawa Stage 6" : 4,
+    "Chapter 12 1st Doujo" : 1,
+    "Chapter 12 2nd Doujo" : 1,
+    "Soul Stage 11" : 1
+}
+
+model.inugami = {
+    "name" : "Inugami",
+    "Chapter 10 2nd Satori" : 1,
+    "Chapter 10 2nd Puppeteer" : 1,
+}
+
+model.honeonna = {
+    "name" : "Hone Onna",
+    "Ame Onna Stage 4" : 4,
+}
+
+model.umibozu = {
+    "name" : "Umibozu",
+    "Chapter 24 1st Vampira" : 3,
+    "Chapter 24 2nd Vampira" : 3,
+    "Umibozu Stage 5" : 1,
+}
+
+model.kyonshiani = {
+    "name" : "Kyonshi Ani",
+    "Chapter 24 1st Jourogumo" : 3,
+    "Chapter 24 2nd Jourogumo" : 3,
+    "Aoandon Stage 6" : 3,
+}
+
+model.puppeteer = {
+    "name" : "Puppeteer",
+}
+
+model.momiji = {
+    "name" : "Momiji",
+    "Momiji Stage 1" : 1,
+    "Kappa Stage 5" : 1,
+}
+
+model.hangan = {
+    "name" : "Hangan",
+    "Soul Stage 8" : 1,
+}
+
+model.sakura = {
+    "name" : "Sakura",
+    "Chapter 8 1st Sakura" : 1,
+    "Chapter 8 2nd Sakura" : 1,
+    "Chapter 8 Boss Sakura" : 1,
+    "Kappa Stage 5" : 1,
+}
+
+model.yumekui = {
+    "name" : "Yumekui",
+    "Chapter 14 Boss Yumekui" : 5,
+    "Soul Stage 4" : 2,
+}
+
+model.hououka = {
+    "name" : "Hououka",
+    "Chapter 20 2nd Hannya" : 1,
+    "Soul Stage 7" : 1,
+    "Kappa Stage 5" : 1,
+}
+
+model.momo = {
+    "name" : "Momo",
+    "Soul Stage 3" : 1,
+}
+
+model.mouba = {
+    "name" : "Mouba",
+    "Soul Stage 5" : 2,
+    "Chapter 23 1st Mouba" : 1,
+    "Chapter 23 2nd Mouba" : 1,
+    "Chapter 23 3rd Mouba" : 1,
+}
+
+model.vampira = {
+    "name" : "Vampira",
+    "Chapter 21 1st Vampira" : 1,
+    "Chapter 21 2nd Vampira" : 1,
+    "Chapter 24 1st Vampira" : 1,
+    "Chapter 24 2nd Vampira" : 1,
+}
+
+model.kuromujou = {
+    "name" : "Kuro Mujou",
+    "Kappa Stage 6" : 3,
+    "Chapter 24 1st Kiyohime" : 2,
+    "Chapter 24 2nd Kiyohime" : 2,
+    "Chapter 24 3rd Kiyohime" : 2,
+}
+
+model.shiromujou = {
+    "name" : "Shiro Mujou",
+    "Ootengu Stage 6" : 2,
+    "Chapter 24 1st Kiyohime" : 1,
+    "Chapter 24 2nd Kiyohime" : 1,
+    "Chapter 24 3rd Kiyohime" : 1,
+}
+
+model.kamaitachi = {
+    "name" : "Kamaitachi",
+    "Soul Stage 5" : 1,
+    "Kappa Stage 4" : 1,
+}
+
+model.youko = {
+    "name" : "Youko",
+    "Chapter 7 Boss Youko" : 2,
+    "Soul Sougenbi" : 1,
+}
+
+model.ootengu = {
+    "name" : "Ootengu",
+    "Soul Stage 10" : 2,
+    "Soul Stage 11" : 2,
+    "Chapter 15 2nd Lantern Boy" : 1,
+}
+
+model.shutendoji = {
+    "name" : "Shuten Doji",
+    "Soul Stage 10" : 1,
+}
+
+model.arakawalord = {
+    "name" : "Arakawa Lord",
+    "Soul Stage 7" : 2,
+}
+
+model.enma = {
+    "name" : "Enma",
+    "Soul Stage 6" : 1,
+}
